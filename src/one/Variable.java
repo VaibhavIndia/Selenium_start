78586fa2c910fe9ba0f0bc1ff4c02d535f975c4d
VaibhavIndia <vaibhav.nigade@gmail.com>
2018-06-10 11:43:24 +0530

    sunday checkin

---
+++ src/one/Variable.java
@@ -0,0 +1,18 @@
+package one;
+
+public class Variable {
+
+	public static void main(String[] args) 
+	{
+		boolean a = true;
+		System.out.println(a);
+		char b =  'M';
+		System.out.println(b);
+		short n =1032;
+		System.out.println(n);
+		
+		float m = 323.32f;
+		System.out.println(m);
+	}
+
+}
